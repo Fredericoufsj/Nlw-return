92e638ef3232d4728f13d34bef2dad4e4a844b15
Frederico Thomaz <fredericoufsj@yahoo.com.br>
2022-05-03 13:46:34 -0300

    Aula-1 finalizada

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,6 @@
+import { Widget } from "./components/widget";
 
-interface ButtonProps{
-  text?:string;
+
+export function App(){
+  return <Widget />
 }
-
-function Button(props:ButtonProps){
-  return(
-    <button className="bg-violet-500 p-2">{props.text ?? 'Default'}</button>
-  )
-}
-
-
-
-function App() {
- return(
-    <div>
-      <h1>Hello World</h1>
-      <Button text="Send"/>
-      <Button text="OK"/>
-      <Button />
-    </div>
- )
-}
-
-export default App;
